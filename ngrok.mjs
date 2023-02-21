811e51cfcd4e10fae5501f87691a4ef136000ead
test <test>
2023-02-21 01:19:50 +0000

    fix ngrok

--- ngrok.mjs
+++ ngrok.mjs
@@ -1,3 +1,3 @@
 //// Ngrok
 // run a docker container on a dynamic port with a subdomain
-await $`docker run --net=host -p 4040 -it ngrok/ngrok http 80 --subdomain ${$.env.SUBDOMAIN}`
+await $`docker run --rm -d --net=host -p 4040 -it ngrok/ngrok http 80 --subdomain ${$.env.SUBDOMAIN}`
